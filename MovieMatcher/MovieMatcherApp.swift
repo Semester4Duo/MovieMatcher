53ce0a9a679004c136ae4f51dbb6020cdd9b69d0
Dylan Nas <KurzaCationer@gmail.com>
2021-09-24 10:25:54 +0200

    Initial Commit

---
+++ MovieMatcher/MovieMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMatcherApp.swift
+//  MovieMatcher
+//
+//  Created by Dylan Nas on 24/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct MovieMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
