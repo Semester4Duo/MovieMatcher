85ef3bc7066d6cf7d862e5388f7bca912e732908
David <david.r.rozenberg@gmail.com>
2021-10-01 14:33:05 +0200

    Login view
    Added login view

--- MovieMatcher/Views/Pages/LoginView.swift
+++ MovieMatcher/Views/Pages/LoginView.swift
@@ -8,10 +8,88 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        ZStack{
-        BackgroundView()
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader{ geometry in
+            ZStack{
+                BackgroundView()
+                VStack{
+                    Spacer()
+                    Group{
+                        Text("Log in").titleText()
+                        TextField("", text: $email)
+                            .placeholder(when: email.isEmpty){
+                                Text("Email")
+                                    .foregroundColor(.white)
+                                    .zIndex(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                                    .padding(.leading)
+                                    .opacity(0.6)
+                                    
+                            }
+                            .textFieldStyle(OvalTextFieldStyle())
+                            .padding(.vertical)
+                            .padding(.horizontal, 25)
+                            .foregroundColor(.white)
+                            
+                        
+                        SecureField("password", text: $email)
+                            .placeholder(when: email.isEmpty){
+                                Text("Password")
+                                    .foregroundColor(.white)
+                                    .zIndex(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                                    .padding(.leading)
+                                    .opacity(0.6)
+                                    
+                            }
+                            .textFieldStyle(OvalTextFieldStyle())
+                            .padding(.horizontal, 25)
+                            .padding(.bottom)
+                        
+                        Button("Sign in"){
+                            
+                        }
+                        .frame(width: geometry.size.width - 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .buttonStyle()
+                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        
+                        Button("Forgot password"){
+                            
+                        }
+                    }
+                    
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Group{
+                        Text("Or connect with")
+                            .foregroundColor(.white)
+                            .padding(.bottom, 10)
+                        HStack{
+                            Button("Apple"){
+                                
+                            }
+                            .frame(width: geometry.size.width/2-30, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .buttonStyle()
+                            Button("Other options"){
+                                
+                            }
+                            .frame(width: geometry.size.width/2-30, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .buttonStyle()
+                        }
+                        .opacity(0.8)
+                        HStack{
+                            Text("Dont have a account?")
+                                .foregroundColor(.white)
+                            Button("sign up"){
+                                
+                            }
+                        }
+                    }
+                    Spacer()
+                }
+            }
         }
     }
 }
--- Styles.swift
+++ Styles.swift
@@ -17,6 +17,18 @@ extension View{
         modifier(TitleStyle())
     }
 }
+extension View{
+    func inputStyle() -> some View{
+        modifier(InputStyle())
+    }
+}
+
+extension View{
+    func buttonStyle() -> some View{
+        modifier(ButtonStyle())
+    }
+}
+
 
 struct BackgroundView: View {
     var body: some View {
@@ -43,6 +55,26 @@ struct DefaultText: ViewModifier{
     }
 }
 
+struct InputStyle: ViewModifier{
+    func body(content: Content) -> some View{
+        content
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding(.horizontal, 15)
+            .padding(.bottom,10)
+    }
+}
+
+struct ButtonStyle: ViewModifier{
+    func body(content: Content) -> some View{
+        content
+            .background(LinearGradient(gradient: Gradient(colors: [Color("Purple1"), Color("Purple2")]), startPoint: .top, endPoint: .bottom))
+            .clipShape(Capsule(), style: /*@START_MENU_TOKEN@*/FillStyle()/*@END_MENU_TOKEN@*/)
+            .padding(.bottom,5)
+            .foregroundColor(.white)
+    }
+}
+
+
 struct TabLabelStyle: LabelStyle {
     func makeBody(configuration: Configuration) -> some View {
         Label(configuration)
@@ -50,5 +82,28 @@ struct TabLabelStyle: LabelStyle {
             .background(Color.yellow)
             .foregroundColor(Color.red)
     
+    }
+}
+
+struct OvalTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(10)
+            .background(Color("InputField"))
+            .cornerRadius(20)
+    }
+}
+
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View{
+        
+        ZStack(alignment: alignment){
+            self
+            placeholder().opacity(shouldShow ? 1 : 0)
+        }
     }
 }
